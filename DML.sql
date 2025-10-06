7691e7344175994a6d2dbc3d595137d2d6f1486b
Islam Alhallak <113602370+PotatoQwerty@users.noreply.github.com>
2025-10-06 15:13:33 +0000

    ADDED ANOTHER CHECKPOINT FOR DML

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+    Products
+Values
+    ('P01', 'Samsung Galaxy S20', 3299, 'Smartphone'),
+    ('P02', 'ASUS NoteBook', 4599, 'Pc');
+
+INSERT INTO
+    Customers (Customer_ID, Customer_Name, Customer_Tel)
+Values
+    ('C01', 'Ali', '0123456789'),
+    ('C02', 'ASMA', '0987654321');
+
+INSERT INTO
+    Orders
+Values
+    ('C01', 'P02', 2, 9198, NULL),
+    ('C02', 'P01', 1, 3299, '28/05/2020');
